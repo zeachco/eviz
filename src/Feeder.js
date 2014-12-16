b8d9a1e65eb7f75510c30c152f63d0fef68cbc4b
Olivier Rousseau-Guyot <zeachco@gmail.com>
2014-12-15 21:52:57 -0500

    feeder functionnality

--- src/Feeder.js
+++ src/Feeder.js
@@ -1,16 +1,24 @@
 define(['d3'], function(d3){
   'use strict';
-  return function Feeder(func, maps, length){
-    length = length || 1000;
-    var data = d3.range(length);
-    maps = maps || [];
-    data.map(function(d, i){
-      var arr = [];
-      maps.forEach(function(f, i){
-        arr[i] = f(Math.random(), i);
+  return function Feeder(func, maps, len, speed){
+    speed = speed || 1000;
+    var data = [];
+
+    function feed(){
+      data = d3.range(len);
+      len = len || 1000;
+      maps = maps || [];
+      data = data.map(function(d, i){
+        var arr = [];
+        maps.forEach(function(f, i){
+          arr[i] = f(Math.random(), i, arr);
+        });
+        return arr;
       });
-      return arr;
-    });
-    func(data);
+      func(data);
+
+      setTimeout(feed, speed);
+    }
+    feed();
   };
 });
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var template;
-define(['d3', 'Display'], function(d3, Viz){
+define(['d3', 'Display', 'Feeder'], function(d3, Viz, Feeder){
   'use strict';
 
   var container = d3.select('body').append('div').node();
@@ -9,6 +9,12 @@ define(['d3', 'Display'], function(d3, Viz){
   });
   template.init();
 
+  new Feeder(template.update, [
+    function(r, i, arr){ return r*1000; },
+    function(r, i, arr){ return arr[0] + r*10; },
+    function(r, i, arr){ return r*10; }
+  ], 1000 );
+/*
   function feed(){
     var data = d3.range(50)
       .filter(function(d){
@@ -22,5 +28,5 @@ define(['d3', 'Display'], function(d3, Viz){
     template.update(data);
     setTimeout(feed, 1750);
   }
-  feed();
+  feed();*/
 });
