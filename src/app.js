e2d6104a86319e632c71e188d90bbdd5a511e530
Olivier Rousseau-Guyot <zeachco@gmail.com>
2014-12-15 21:53:25 -0500

    clean

--- src/app.js
+++ src/app.js
@@ -14,19 +14,4 @@ define(['d3', 'Display', 'Feeder'], function(d3, Viz, Feeder){
     function(r, i, arr){ return arr[0] + r*10; },
     function(r, i, arr){ return r*10; }
   ], 1000 );
-/*
-  function feed(){
-    var data = d3.range(50)
-      .filter(function(d){
-        var r = Math.ceil(Math.random()*10);
-        return d % r === 0;
-      })
-      .map(function(d){
-        return { index: d, r: Math.random()*100 };
-      })
-    ;
-    template.update(data);
-    setTimeout(feed, 1750);
-  }
-  feed();*/
 });
